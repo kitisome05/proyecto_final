a10890999f6718f8c4dc7993885c8186bd2aa62c
kitisome05 <kitisome05@gmail.com>
2018-05-09 15:38:48 +0200

    Avance google chart

---
+++ consultas_file/script-consultas.sh
@@ -0,0 +1,3 @@
+osqueryi --json "select * from acpi_tables;" > /var/www/html/proyecto/proyecto_final/consultas_file/acpi_tables.json
+osqueryi --json "select * from apt_sources;" > /var/www/html/proyecto/proyecto_final/consultas_file/apt_sources.json
+osqueryi --json "select * from arp_cache;" > /var/www/html/proyecto/proyecto_final/consultas_file/arp_cache.json
