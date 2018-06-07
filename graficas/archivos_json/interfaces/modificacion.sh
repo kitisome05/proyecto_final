4ba871df5e6e8a4d9d1be61fefced507c887ff8f
kitisome05 <kitisome05@gmail.com>
2018-06-07 15:26:21 +0200

    avance de tablas 1

--- graficas/archivos_json/interfaces/modificacion.sh
+++ graficas/archivos_json/interfaces/modificacion.sh
@@ -2,3 +2,4 @@ cat dispositivosE-S.json |tr ',' ':' |tr -d '{'|tr -d '}'|tr -d '"'|tr -d '['|tr
 cat prioridad-baja.json |tr ',' ':' |tr -d '{'|tr -d '}'|tr -d '"'|tr -d '['|tr -d ']'>prioridad-baja2.json
 cat system.json |tr ',' ':' |tr -d '{'|tr -d '}'|tr -d '"'|tr -d '['|tr -d ']'>system2.json
 cat users.json |tr ',' ':' |tr -d '{'|tr -d '}'|tr -d '"'|tr -d '['|tr -d ']'>users2.json
+cat tabla-ejemplo.json | sed 's/""/##/g'  |tr ',' ':' |tr -d '{'|tr -d '}'|tr -d '"'|tr -d '['|tr -d ']'>tabla-ejemplo33.json
