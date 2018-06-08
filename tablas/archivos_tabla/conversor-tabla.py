5df3bc65de48381a411856b7838e25cef6ffd8ec
kitisome05 <kitisome05@gmail.com>
2018-06-09 00:48:39 +0200

    Todas las tablas creadas a falta de poner en pagina principal

--- tablas/archivos_tabla/conversor-tabla.py
+++ tablas/archivos_tabla/conversor-tabla.py
@@ -42,7 +42,7 @@ while linea:
         	#print lcampo
 		f2.write(lcampo)
 	linea=fi.readline()
-        print linea
+        #print linea
 linea2="]);\n var table = new google.visualization.Table(document.getElementById('table_div')); \n table.draw(data, {showRowNumber: true, width: '100%', height: '100%'}); \n } \n </script> \n </head>  \n <body>  \n <div id=\"table_div\"></div> \n </body> \n </html>  \n"
 #linea2="<html>\"\n\"<head>\"\n\"<script type=\"text/javascript\" src=\"https://www.gstatic.com/charts/loader.js\"></script>\"\n\"<script type=\"text/javascript\">\"\n\"google.charts.load('current', {'packages':['table']});\"\n\"google.charts.setOnLoadCallback(drawTable); function drawTable() { var data = new google.visualization.DataTable();"
 f2.write(linea2)
