b4957f3eee2747d8ab6295249741df03b4e0bee6
kitisome05 <kitisome05@gmail.com>
2018-06-08 20:53:55 +0200

    Avance en las tablas creadas, falta el html de las tablas

--- script-consultas.sh
+++ script-consultas.sh
@@ -25,138 +25,137 @@ osqueryi --json "select interface, mac from interface_details;" > /var/www/html/
 osqueryi --json "select name, size from kernel_modules order by size desc limit 5;" > /var/www/html/proyecto/proyecto_final/graficas/archivos_json/modules/size_modules.json
 
 #Consultas Generales
-osqueryi --json "select * from acpi_tables;" > /var/www/html/proyecto/proyecto_final/archivos_json/acpi_tables.json
-osqueryi --json "select * from apt_sources;" > /var/www/html/proyecto/proyecto_final/archivos_json/apt_sources.json
+osqueryi --json "select * from acpi_tables;" 
+osqueryi --json "select * from apt_sources;" 
 	#Interfaces de red
-osqueryi --json "select * from apt_sources;" > /var/www/html/proyecto/proyecto_final/archivos_json/apt_sources.json
+osqueryi --json "select * from apt_sources;" 
 	#...
-osqueryi --json "select * from augeas;" > /var/www/html/proyecto/proyecto_final/archivos_json/augeas.json
+osqueryi --json "select * from augeas;" 
 	#....Tabla de claves de archivos
-osqueryi --json "select * from authorized_keys;" > /var/www/html/proyecto/proyecto_final/archivos_json/authorized_keys.json
+osqueryi --json "select * from authorized_keys;" 
 	#...seguridad
-osqueryi --json "select * from carbon_black_info;" > /var/www/html/proyecto/proyecto_final/archivos_json/carbon_black_info.json
+osqueryi --json "select * from carbon_black_info;" 
 	#...Herramienta para recuperar archivos dañados.
-osqueryi --json "select * from carves;" > /var/www/html/proyecto/proyecto_final/archivos_json/carves.json
+osqueryi --json "select * from carves;" 
 	#.Funciones de la CPU
-osqueryi --json "select * from cpuid;" > /var/www/html/proyecto/proyecto_final/archivos_json/cpuid.json
+osqueryi --json "select * from cpuid;" 
 	#.Eventos realizados en el sistema
-osqueryi --json "select * from crontab;" > /var/www/html/proyecto/proyecto_final/archivos_json/crontab.json
-###################################    ME HE QUEDADO AQUI LINDO
+osqueryi --json "select * from crontab;" 
 	#...Registro de solicitudes http
-osqueryi --json "select * from curl;" > /var/www/html/proyecto/proyecto_final/archivos_json/curl.json
+osqueryi --json "select * from curl;" 
 	#...Inspeccion de los certificados
-osqueryi --json "select * from curl_certificate;" > /var/www/html/proyecto/proyecto_final/archivos_json/curl_certificate.json
+osqueryi --json "select * from curl_certificate;" 
 	#.Encriptaciones de disco
-osqueryi --json "select * from disk_encryption;" > /var/www/html/proyecto/proyecto_final/archivos_json/disk_encryption.json
+osqueryi --json "select * from disk_encryption;" 
 	#Resolucion de dns
-osqueryi --json "select * from dns_resolvers;" > /var/www/html/proyecto/proyecto_final/archivos_json/dns_resolvers.json
+osqueryi --json "select * from dns_resolvers;" 
 	#...Contenedores en el sistema
-osqueryi --json "select * from docker_container_labels;" > /var/www/html/proyecto/proyecto_final/archivos_json/docker_container_labels.json
+osqueryi --json "select * from docker_container_labels;" 
 	#...Contenedores que estan montados
-osqueryi --json "select * from docker_container_mounts;" > /var/www/html/proyecto/proyecto_final/archivos_json/docker_container_mounts.json
+osqueryi --json "select * from docker_container_mounts;" 
 	#...Red de los contenedores
-osqueryi --json "select * from docker_container_networks;" > /var/www/html/proyecto/proyecto_final/archivos_json/docker_container_networks.json
+osqueryi --json "select * from docker_container_networks;" 
 	#...Puerto de los contenedores
-osqueryi --json "select * from docker_container_ports;" > /var/www/html/proyecto/proyecto_final/archivos_json/docker_container_ports.json
+osqueryi --json "select * from docker_container_ports;" 
 	#...Instancia de metadatos
-osqueryi --json "select * from ec2_instance_metadata;" > /var/www/html/proyecto/proyecto_final/archivos_json/ec2_instance_metadata.json
+osqueryi --json "select * from ec2_instance_metadata;" 
 	#/etc/hosts nos muestra dns
-osqueryi --json "select * from etc_hosts;" > /var/www/html/proyecto/proyecto_final/archivos_json/etc_hosts.json
+osqueryi --json "select * from etc_hosts;" 
 	#Protocolos del sistema
-osqueryi --json "select * from etc_protocols;" > /var/www/html/proyecto/proyecto_final/archivos_json/etc_protocols.json
+osqueryi --json "select * from etc_protocols;" 
 	#Servicios del sistema
-osqueryi --json "select * from etc_services;" > /var/www/html/proyecto/proyecto_final/archivos_json/etc_services.json
+osqueryi --json "select * from etc_services;" 
 	#...Atributos y metadatos de archivos del sistema
-osqueryi --json "select * from file;" > /var/www/html/proyecto/proyecto_final/archivos_json/file.json
+osqueryi --json "select * from file;" 
 	#...Eventos de archivos modificados que tenemos configurado
-osqueryi --json "select * from file_events;" > /var/www/html/proyecto/proyecto_final/archivos_json/file_events.json
+osqueryi --json "select * from file_events;" 
 	#Extenciones de firefox
-osqueryi --json "select * from firefox_addons;" > /var/www/html/proyecto/proyecto_final/archivos_json/firefox_addons.json
+osqueryi --json "select * from firefox_addons;" 
 	#Grupos del sistema
-osqueryi --json "select * from groups;" > /var/www/html/proyecto/proyecto_final/archivos_json/groups.json
+osqueryi --json "select * from groups;" 
 	#...Eventos de hardware como USB, PCI...
-osqueryi --json "select * from hardware_events;" > /var/www/html/proyecto/proyecto_final/archivos_json/hardware_events.json
+osqueryi --json "select * from hardware_events;" 
 	#...Sistema de archivos de Hash de datos
-osqueryi --json "select * from hash;" > /var/www/html/proyecto/proyecto_final/archivos_json/hash.json
+osqueryi --json "select * from hash;" 
 	#...Version e info del procesador intel
-osqueryi --json "select * from intel_me_info;" > /var/www/html/proyecto/proyecto_final/archivos_json/intel_me_info.json
+osqueryi --json "select * from intel_me_info;" 
 	#Direccion de las interfaces
-osqueryi --json "select * from interface_addresses;" > /var/www/html/proyecto/proyecto_final/archivos_json/interface_addresses.json
+osqueryi --json "select * from interface_addresses;" 
 	#Detalles de las interfaces- paquetes de salida y entrada
-osqueryi --json "select * from interface_details;" > /var/www/html/proyecto/proyecto_final/archivos_json/interface_details.json
+osqueryi --json "select * from interface_details;" 
 	#...Iptables
-osqueryi --json "select * from iptables;" > /var/www/html/proyecto/proyecto_final/archivos_json/iptables.json
+osqueryi --json "select * from iptables;" 
 	#Informacion del kernel
-osqueryi --json "select * from kernel_info;" > /var/www/html/proyecto/proyecto_final/archivos_json/kernel_info.json
+osqueryi --json "select * from kernel_info;" 
 	#Los ultimos inicio y cierre de sesion 
-osqueryi --json "select * from last;" > /var/www/html/proyecto/proyecto_final/archivos_json/last.json
+osqueryi --json "select * from last;" 
 	#Puertos que estan escuchando
-osqueryi --json "select * from listening_ports;" > /var/www/html/proyecto/proyecto_final/archivos_json/listening_ports.json
+osqueryi --json "select * from listening_ports;" 
 	#Redes vecinas
-osqueryi --json "select * from lldp_neighbors;" > /var/www/html/proyecto/proyecto_final/archivos_json/lldp_neighbors.json
+osqueryi --json "select * from lldp_neighbors;" 
 	#.Periodo de inicio del sistema
-osqueryi --json "select * from load_average;" > /var/www/html/proyecto/proyecto_final/archivos_json/load_average.json
+osqueryi --json "select * from load_average;" 
 	#Usiarios logeados en el sistema
-osqueryi --json "select * from logged_in_users;" > /var/www/html/proyecto/proyecto_final/archivos_json/logged_in_users.json
+osqueryi --json "select * from logged_in_users;" 
 	#...Configuraci{on de raid por drivers
-osqueryi --json "select * from md_drives;" > /var/www/html/proyecto/proyecto_final/archivos_json/md_drives.json
+osqueryi --json "select * from md_drives;" 
 	#Raids que soporta nuestro kernel
-osqueryi --json "select * from md_personalities;" > /var/www/html/proyecto/proyecto_final/archivos_json/md_personalities.json
+osqueryi --json "select * from md_personalities;" 
 	#Informacion de memoria
-osqueryi --json "select * from memory_info;" > /var/www/html/proyecto/proyecto_final/archivos_json/memory_info.json
+osqueryi --json "select * from memory_info;" 
 	#Mapa de la memoria del sistema
-osqueryi --json "select * from memory_map;" > /var/www/html/proyecto/proyecto_final/archivos_json/memory_map.json
+osqueryi --json "select * from memory_map;" 
 	#Dispositivos y sistemas de archivos montados en el sistema
-osqueryi --json "select * from mounts;" > /var/www/html/proyecto/proyecto_final/archivos_json/mounts.json
+osqueryi --json "select * from mounts;" 
 	#Version del sistema operativo
-osqueryi --json "select * from os_version;" > /var/www/html/proyecto/proyecto_final/archivos_json/os_version.json
+osqueryi --json "select * from os_version;" 
 	#Eventos de osquery
-osqueryi --json "select * from osquery_events;" > /var/www/html/proyecto/proyecto_final/archivos_json/osquery_events.json
+osqueryi --json "select * from osquery_events;" 
 	#Opciones que modifican el comportamiento de osquery
-osqueryi --json "select * from osquery_flags;" > /var/www/html/proyecto/proyecto_final/archivos_json/osquery_flags.json
+osqueryi --json "select * from osquery_flags;" 
 	#Informacion de osquery
-osqueryi --json "select * from osquery_info;" > /var/www/html/proyecto/proyecto_final/archivos_json/osquery_info.json
+osqueryi --json "select * from osquery_info;" 
 	#.Pauetes de osquery
-osqueryi --json "select * from osquery_packs;" > /var/www/html/proyecto/proyecto_final/archivos_json/osquery_packs.json
+osqueryi --json "select * from osquery_packs;" 
 	#.Complementos de registro de osquery
-osqueryi --json "select * from osquery_registry;" > /var/www/html/proyecto/proyecto_final/archivos_json/osquery_registry.json
+osqueryi --json "select * from osquery_registry;" 
 	#.Informacion sobre las consultas programadas
-osqueryi --json "select * from osquery_schedule;" > /var/www/html/proyecto/proyecto_final/archivos_json/osquery_schedule.json
+osqueryi --json "select * from osquery_schedule;" 
 	#Dispositivos PCI activos en el sistema
-osqueryi --json "select * from pci_devices;" > /var/www/html/proyecto/proyecto_final/archivos_json/pci_devices.json
+osqueryi --json "select * from pci_devices;" 
 	#Informacion sobre uefi y pataforma de arranque 
-osqueryi --json "select * from platform_info;" > /var/www/html/proyecto/proyecto_final/archivos_json/platform_info.json
+osqueryi --json "select * from platform_info;" 
 	#.Tabla clave de variables de entornos de proceso
-osqueryi --json "select * from process_envs;" > /var/www/html/proyecto/proyecto_final/archivos_json/process_envs.json
+osqueryi --json "select * from process_envs;" 
 	#Archivos de memoria de procesos mapeados
-osqueryi --json "select * from process_memory_map;" > /var/www/html/proyecto/proyecto_final/archivos_json/process_memory_map.json
+osqueryi --json "select * from process_memory_map;" 
 	#Ruta para procesos abiertos
-osqueryi --json "select * from process_open_files;" > /var/www/html/proyecto/proyecto_final/archivos_json/process_open_files.json
+osqueryi --json "select * from process_open_files;" 
 	#Procesos que tienen conectores de red abierto en el sistema
-osqueryi --json "select * from process_open_sockets;" > /var/www/html/proyecto/proyecto_final/archivos_json/process_open_sockets.json
+osqueryi --json "select * from process_open_sockets;" 
 	#Paquetes instalados de Python
-osqueryi --json "select * from python_packages;" > /var/www/html/proyecto/proyecto_final/archivos_json/python_packages.json
+osqueryi --json "select * from python_packages;" 
 	#Tabla de rutas 
-osqueryi --json "select * from routes;" > /var/www/html/proyecto/proyecto_final/archivos_json/routes.json
+osqueryi --json "select * from routes;" 
 	#.Contraseñas encriptadas
-osqueryi --json "select * from shadow;" > /var/www/html/proyecto/proyecto_final/archivos_json/shadow.json
+osqueryi --json "select * from shadow;" 
 	#Compartir memoria
-osqueryi --json "select * from shared_memory;" > /var/www/html/proyecto/proyecto_final/archivos_json/shared_memory.json
+osqueryi --json "select * from shared_memory;" 
 	#Historial del shell-Ultimas consultas
-osqueryi --json "select * from shell_history;" > /var/www/html/proyecto/proyecto_final/archivos_json/shell_history.json
+osqueryi --json "select * from shell_history;" 
 	#Detalles y contenido de la BIOS
-osqueryi --json "select * from smbios_tables;" > /var/www/html/proyecto/proyecto_final/archivos_json/smbios_tables.json
+osqueryi --json "select * from smbios_tables;" 
 	#Sudoers
-osqueryi --json "select * from sudoers;" > /var/www/html/proyecto/proyecto_final/archivos_json/sudoers.json
+osqueryi --json "select * from sudoers;" 
 	#Permisos binarios de grupo
-osqueryi --json "select * from suid_bin;" > /var/www/html/proyecto/proyecto_final/archivos_json/suid_bin.json
+osqueryi --json "select * from suid_bin;" 
 	#Informacion del sistema
-osqueryi --json "select * from system_info;" > /var/www/html/proyecto/proyecto_final/archivos_json/system_info.json
+osqueryi --json "select * from system_info;" 
 	#Tiempo
-osqueryi --json "select * from time;" > /var/www/html/proyecto/proyecto_final/archivos_json/time.json
+osqueryi --json "select * from time;" 
 	#Tiempo de encendido
-osqueryi --json "select * from uptime;" > /var/www/html/proyecto/proyecto_final/archivos_json/uptime.json
+osqueryi --json "select * from uptime;" 
 	#Usuarios
-osqueryi --json "select * from users;" > /var/www/html/proyecto/proyecto_final/archivos_json/users.json
+osqueryi --json "select * from users;" 
 	#Nombre de aplicaciones puerto y pid
-osqueryi --json "select DISTINCT process.name, listening.port, process.pid from processes as process join listening_ports as listening on process.pid = listening.pid where listening.address = '0.0.0.0';" > /var/www/html/proyecto/proyecto_final/archivos_json/app.json
+osqueryi --json "select DISTINCT process.name, listening.port, process.pid from processes as process join listening_ports as listening on process.pid = listening.pid where listening.address = '0.0.0.0';" 
