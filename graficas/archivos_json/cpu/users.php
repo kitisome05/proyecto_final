28418be93bebbc0201e2e38da060e74c5b4791bd
kitisome05 <kitisome05@gmail.com>
2018-06-11 17:16:21 +0200

    Terminado

--- graficas/archivos_json/cpu/users.php
+++ graficas/archivos_json/cpu/users.php
@@ -1,11 +1,6 @@
 <?php 
 
-// This is just an example of reading server side data and sending it to the client.
-// It reads a json formatted text file and outputs it.
-
-$string = file_get_contents("cusers2.json");
-echo $string;
-
-// Instead you can query your database and parse into JSON etc etc
+    $string = file_get_contents("cusers2.json");
+    echo $string;
 
 ?>
